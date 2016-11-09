77c9009c5dda80f3aadfb5e4636fcd21544f2464
Thai Thien <tthien@apcs.vn>
2016-11-10 00:57:27 +0700

    init

---
+++ main.lua
@@ -0,0 +1,135 @@
+require 'torch'
+require 'nn'
+require 'nngraph'
+require 'optim'
+require 'model.SimpleRNN'
+require 'util.misc' -- share_params
+require 'optim'
+
+local CharSplitLMMinibatchLoader = require 'util.CharSplitLMMinibatchLoader'
+
+-- opt
+opt = {}
+opt.data_dir = 'data/tinyshakespeare'
+opt.batch_size =50
+opt.seq_length =50
+opt.train_frac =0.95
+opt.val_frac =0.05
+
+opt.rnn_size = 128
+
+
+ test_frac = math.max(0, 1 - (opt.train_frac + opt.val_frac))
+ split_sizes = {opt.train_frac, opt.val_frac, test_frac}
+
+
+-- load data
+ loader = CharSplitLMMinibatchLoader.create(opt.data_dir, opt.batch_size, opt.seq_length, split_sizes)
+ vocab_size = loader.vocab_size  -- the number of distinct characters
+ vocab = loader.vocab_mapping
+print('vocab size: ' .. vocab_size)
+opt.input_size = vocab_size
+-- 1: train 2:val 3: text
+-- loader:next_batch(num)
+
+-- master cell for all other cell to share params with
+master_cell = RNN.create(opt)
+params, gradParams = master_cell:getParameters()
+
+-- create a rnn chain
+model = {}
+criterion = {}
+for i = 1,opt.seq_length do
+  local cell = RNN.create(opt)
+  local crit = nn.ClassNLLCriterion()
+  share_params(cell,master_cell)
+  table.insert(model, cell)
+  table.insert(criterion,crit)
+end
+
+-- feval
+function feval(params)
+  local predict ={} -- y^
+  local h = {} -- h
+  local loss = 0
+
+    --
+    -- x data
+    -- y label
+    x, y = loader:next_batch(1)
+    x, y = prepro(x,y)
+    h_0 = torch.Tensor(opt.rnn_size):zero()
+    h[0] = h_0
+
+    -- forward pass
+    for t = 1, opt.seq_length do
+      local output = model[t]:forward({x[t], h[t-1]})
+      h[t] = output[1]
+      predict[t] = output[2]
+      loss = loss + criterion[t]:forward(predict[t],y[t])
+    end
+
+    -- backward pass
+    local dh = {}
+    dh[opt.seq_length] = h_0
+    for t = opt.seq_length, 1, -1 do
+      local doutput = criterion[t]:backward(predict[t], y[t])
+      local d = model[t]:backward({x[t],h[t-1]},{dh[t],doutput})
+      dh[t-1] = d[2]
+    end
+    loss = loss / opt.seq_length
+
+    return loss, gradParams
+end
+
+
+
+optimState ={
+  learningRate = 0.01
+}
+
+for epoch = 1, 100 do
+  --optim.sgd(feval,params, optimState)
+end
+
+
+function test()
+  predict ={} -- y^
+  h = {} -- h
+  local loss = 0
+
+    --
+    -- x data
+    -- y label
+    x, y = loader:next_batch(1)
+    x, y = prepro(x,y)
+    h_0 = torch.Tensor(opt.rnn_size):zero()
+    h[0] = h_0
+
+    --forward pass
+    for t = 1, opt.seq_length do
+      local output = model[t]:forward({x[t], h[t-1]}) -- TODO: size missmatch 
+      h[t] = output[1]
+      predict[t] = output[2]
+      print (t)
+      if (t == 2) then
+        print(predict[t])
+        print(y[t])
+      end
+      loss = loss + criterion[t]:forward(predict[t],y[t])
+    end
+
+    -- backward pass
+    local dh = {}
+    dh[opt.seq_length] = h_0
+    for t = opt.seq_length, 1, -1 do
+      local doutput = criterion[t]:backward(predict[t], y[t])
+      local d = model[t]:backward({x[t],h[t-1]},{dh[t],doutput})
+      dh[t-1] = d[2]
+    end
+    loss = loss / opt.seq_length
+
+    return loss, gradParams
+end
+
+test()
---
+++ util/misc.lua
@@ -0,0 +1,39 @@
+
+-- misc utilities
+
+function clone_list(tensor_list, zero_too)
+    -- utility function. todo: move away to some utils file?
+    -- takes a list of tensors and returns a list of cloned tensors
+    local out = {}
+    for k,v in pairs(tensor_list) do
+        out[k] = v:clone()
+        if zero_too then out[k]:zero() end
+    end
+    return out
+end
+
+-- share module parameters
+function share_params(cell, src)
+  if torch.type(cell) == 'nn.gModule' then
+    for i = 1, #cell.forwardnodes do
+      local node = cell.forwardnodes[i]
+      if node.data.module then
+        node.data.module:share(src.forwardnodes[i].data.module,
+          'weight', 'bias', 'gradWeight', 'gradBias')
+      end
+    end
+  elseif torch.isTypeOf(cell, 'nn.Module') then
+    cell:share(src, 'weight', 'bias', 'gradWeight', 'gradBias')
+  else
+    error('parameters cannot be shared for this input')
+  end
+end
+
+--  cessing helper function
+function prepro(x,y)
+    x = x:transpose(1,2):contiguous() -- swap the axes for faster indexing
+    y = y:transpose(1,2):contiguous()
+    x = x:double()
+    y = y:double()
+    return x,y
+end
